8fe0f5973a6d65c90c3fd975558d2cff586bca29
Sanjay Sai <shachsan@hotmail.com>
2019-03-03 13:42:27 -0500

    complete recursion

--- index.js
+++ index.js
@@ -41,21 +41,43 @@ function addUpTo(arr, i){
 
 // addUpTo([4,2,9,5,7], 3)
 
-function maxOf(arr, max=0){
-    // let maxEle;
-    if(arr.length===1 && arr[0]>max){
-        return arr[0];
-    }else if(arr.length===1 && arr[0]< max){
-        return max;
-    }
-    if(arr[0]>=arr[1] && arr[0]>max){
-       maxEle=arr[0]
-    }else if(arr[0]<arr[1] && arr[1]>max){
-        maxEle=arr[1]
-    }
-        let newArr=arr.slice(1)
-        return maxOf(newArr, maxEle)
+// function maxOf(arr, max=0){
+//     // let maxEle;
+//     if(arr.length===1 && arr[0]>max){
+//         return arr[0];
+//     }else if(arr.length===1 && arr[0]< max){
+//         return max;
+//     }
+//     if(arr[0]>=arr[1] && arr[0]>max){
+//        maxEle=arr[0]
+//     }else if(arr[0]<arr[1] && arr[1]>max){
+//         maxEle=arr[1]
+//     }
+//         let newArr=arr.slice(1)
+//         maxOf(newArr, maxEle)
     
+// }
+
+// maxOf([2,6,8,7,5])
+
+function maxOf(arr){
+    if(arr.length>1){
+        let newArr=arr.slice(1)
+        return Math.max(arr[0], maxOf(newArr))
+    }else{
+        return arr[0]
+    }
 }
 
-maxOf([2,6,1,7,5])
+function includesNumber(arr, num){
+    if(arr.length===1 && arr[0]!==num){
+        return false;
+    }
+    
+    if(arr[0]===num){
+        return true;
+    }else{
+        let newArr=arr.slice(1)
+        return includesNumber(newArr,num)
+    }
+}
